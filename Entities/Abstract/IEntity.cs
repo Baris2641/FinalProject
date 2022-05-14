d1ce84a5b02a9cff635dbba9271fe4d0b05ee23a
BARIŞ IŞIK <20202305034@ogr.akdeniz.edu.tr>
2022-05-14 03:11:38 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity İmplement Eden Class Bir Verİtabanı Tablosudur
+   public interface IEntity
+    {
+    }
+}
